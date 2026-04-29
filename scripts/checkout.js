d9cabc9d7f498c357128500fdde2bde1493ffe73
rajeshgit28 <psdrajesh28@gmail.com>
2026-04-29 13:44:32 +0530

    select delivery options by refresh the page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
@@ -11,6 +11,8 @@ const today = dayjs();
 console.log(today);
 const deliveryDate = today.add(7, "days");
 console.log(deliveryDate.format("dddd, MMMM D"));
+
+function renderOrderSummary() {}
 
 let cartSummaryHTML = "";
 
@@ -90,7 +92,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
-    html += ` <div class="delivery-option">
+    html += ` <div class="delivery-option js-delivery-option
+    
+    data-product-id = "${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
                   <input
                     type="radio"
                       ${isChecked ? "checked" : ""}
@@ -119,5 +123,12 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     );
 
     container.remove();
+  });
+});
+
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+  element.addEventListener("click", () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
